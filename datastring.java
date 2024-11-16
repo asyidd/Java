0cbe6391af07410fea732b257bdc65c91691894f
asyidd <wildanhomer0@gmail.com>
2024-11-16 12:57:37 +0700

    Add files via upload

---
+++ datastring.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Input data
+        String firstName = "M.rasyid";
+        String lastName = "ridho";
+
+        // Menggabungkan nama
+        String fullName = firstName + " " + lastName;
+
+        // Menampilkan hasil
+        System.out.println("panjang string pertama :"+firstName.length());
+        System.out.println("First Name: " + firstName);
+        System.out.println("Panjang String kedua : "+lastName.length()); 
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Panjang semua String : "+fullName.length()); 
+
+}
+}
